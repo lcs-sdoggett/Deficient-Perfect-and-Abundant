f638a84f3674ba92d342cc67e0a5deab5dfe5d6e
Scott Doggett <sdoggett@lcs.on.ca>
2020-03-09 11:05:26 -0400

    Initial Commit

---
+++ Deficient, Perfect and Abundant/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Deficient, Perfect and Abundant
+//
+//  Created by Doggett, Scott on 2020-03-09.
+//  Copyright © 2020 Doggett, Scott. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
